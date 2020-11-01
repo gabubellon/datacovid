e60c17d271716e054b1f8cb40f5eef209db317f1
Gabriel Bellon <gabriel.bellon@contaazul.com>
2020-11-01 15:57:01 -0300

    First steps

---
+++ index.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+class Test:
+    def __init__(self): pass
+
+    def initialize(self):
+        st.markdown("""
+            # Isso é apenas um teste     
+        """)
+
+Test().initialize()
